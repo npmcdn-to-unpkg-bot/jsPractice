339fbc0ad7e2575e3510369a513fb5532b02df54
lakerswgq <2452641634@qq.com>
2016-05-16 22:30:21 +0800

    es2015 promise

--- es6/promise.js
+++ es6/promise.js
@@ -1,15 +1,9 @@
-'use strict';
-function timeout(ms){
-	return new Promise(function(reslove,reject){
-		setTimeout(reject, ms);
-	});
+function timeout(ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, ms, 'done');
+  });
 }
-timeout(3000).then(
-	function(){
-		console.log("done");
-	},//success
-	function(){
-		console.log(a);
-		console.log("failed");
-	}//failure
-);
+
+timeout(3000).then((value) => {
+  console.log(value);
+});
