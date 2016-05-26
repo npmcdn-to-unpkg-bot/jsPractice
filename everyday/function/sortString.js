ae2828c37d6017291e64d1e1d249736ed42cf25f
lakerswgq <2452641634@qq.com>
2016-05-26 16:02:24 +0800

    注释增加示例

--- everyday/function/sortString.js
+++ everyday/function/sortString.js
@@ -1,4 +1,6 @@
 // Write a JavaScript function that returns a passed string with letters in alphabetical order
+// Example string : 'webmaster' 
+// Expected Output : 'abeemrstw'
 'use strict';
 String.prototype.sort = function (){
 	return this.split("").sort().join("");
