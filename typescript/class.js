9397afaa58b72a42061679d084714f3bf3e4729b
lakerswgq <2452641634@qq.com>
2016-06-09 12:21:35 +0800

    typescript类继承

--- typescript/class.js
+++ typescript/class.js
@@ -1,12 +1,31 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
 var Person = (function () {
     function Person(name) {
+        if (name === void 0) { name = "nameless"; }
         this.name = name;
-        // code...
     }
     Person.prototype.greet = function () {
         return "hello,my name is " + this.name;
     };
     return Person;
 }());
+var Student = (function (_super) {
+    __extends(Student, _super);
+    function Student(name, classNumber) {
+        if (classNumber === void 0) { classNumber = 9; }
+        _super.call(this, name);
+        this.classNumber = classNumber;
+    }
+    Student.prototype.greet = function () {
+        return _super.prototype.greet.call(this) + " and I am from class " + this.classNumber;
+    };
+    return Student;
+}(Person));
 var julien = new Person("Julien");
 console.log(julien.greet());
+var jack = new Student("jack", 12);
+console.log(jack.greet());
--- typescript/class.ts
+++ typescript/class.ts
@@ -1,7 +1,6 @@
 class Person {
 	
-	constructor( public name: string) {
-		// code...
+	constructor( public name: string = "nameless") {
 	}
 
 	greet (){
@@ -9,5 +8,18 @@ class Person {
 	}
 }
 
+class Student extends Person {
+	classNumber: number;
+	constructor(name: string ,  classNumber: number = 9) {
+		super(name);
+		this.classNumber = classNumber; 
+	}
+	greet (){
+		return super.greet() + " and I am from class " + this.classNumber; 
+	}
+}
+
 let julien = new Person("Julien");
 console.log(julien.greet());
+let jack = new Student("jack", 12);
+console.log(jack.greet());
