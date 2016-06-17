0e09f104097ce1f89acd725c76c80e434a57eab9
lakerswgq <2452641634@qq.com>
2016-06-17 23:01:11 +0800

    手造了一些联系人数据

---
+++ angular2/app/data/contacts.data.ts
@@ -0,0 +1,18 @@
+import { Contact } from "../class/contact";
+
+export var CONTACTS: Contact[] = [
+
+	{
+		name: "Julien",
+		phoneNumber: "13964897627"
+	},
+	{
+		name: "Jack",
+		phoneNumber: "16955467789",
+	},
+	{
+		name: "Billy",
+		phoneNumber: "18922568964"
+	}
+
+];
