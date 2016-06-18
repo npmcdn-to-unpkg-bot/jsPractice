08429e3d8ada2e22f6f3caf5bef239775e1a6681
lakerswgq <2452641634@qq.com>
2016-06-18 22:13:09 +0800

    获取联系人服务

---
+++ angular2/app/service/contacts.service.ts
@@ -0,0 +1,11 @@
+
+import { Contact } from "../class/contact";
+import { CONTACTS } from "../data/contacts.data";
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class ContactService{
+	getContacts (){
+		return CONTACTS;
+	}
+}
