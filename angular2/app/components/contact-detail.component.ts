af05904acdd3035ffd15e81cfb801d8b063da464
lakerswgq <2452641634@qq.com>
2016-06-19 22:59:15 +0800

    联系人详情模块

---
+++ angular2/app/components/contact-detail.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from "@angular/core";
+import { RouteParams } from '@angular/router-deprecated';
+
+import { Contact } from "../class/contact";
+import { ContactsService } from "../service/contacts.service";
+
+@Component({
+	selector : "contact-detail",
+	templateUrl : "app/templates/contact-detail.component.html",
+})
+
+export class ContactDetailComponent implements OnInit{
+	contact: Contact,
+
+	constructor (
+		private contactService: ContactsService,
+		private routeParams: RouteParams
+		){
+
+	},
+	ngOnInit (){
+		let id = this.routeParams.get("id");
+		this.contact = this.contactService.getContact(id);
+	}
+
+}
