6841b4410b3e6ac6b90cb9d85faa45229f16a345
lakerswgq <2452641634@qq.com>
2016-06-19 19:42:39 +0800

    主模块导入所需的类

--- angular2/app/components/app.component.ts
+++ angular2/app/components/app.component.ts
@@ -1,8 +1,16 @@
 import { Component } from "@angular/core";
 
+import { Contact } from "../class/contact";
+import { ContactsService } from "../service/contacts.service";
+import { ContactsComponent } from "./contacts.component";
+
 @Component({
 	selector : "my-app",
-	template : "<h1>My first angular 2 app</h1>"
+	template : `<h1>My first angular 2 app</h1>
+			<contacts></contacts>
+			`,
+	directives : [ContactsComponent],
+	providers : [ContactsService]
 })
 
 export class AppComponent{
