9b0d9eba3cdd71d88d5d2b9d56b5b8be8c8d558d
lakerswgq <2452641634@qq.com>
2016-06-20 19:51:57 +0800

    主模块路由设置

--- angular2/app/components/app.component.ts
+++ angular2/app/components/app.component.ts
@@ -1,18 +1,41 @@
 import { Component } from "@angular/core";
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+
 
 import { Contact } from "../class/contact";
 import { ContactsService } from "../service/contacts.service";
 import { ContactsComponent } from "./contacts.component";
+import { ContactDetailComponent } from "./contact-detail.component";
 
 @Component({
 	selector : "my-app",
 	template : `<h1>My first angular 2 app</h1>
-			<contacts></contacts>
+			<router-outlet></router-outlet>
 			`,
 	styleUrls : ["app/style/style.css"],
-	directives : [ContactsComponent],
-	providers : [ContactsService]
+	directives : [
+		ROUTER_DIRECTIVES,
+	],
+	providers : [
+			ContactsService,
+			ROUTER_PROVIDERS,
+	],
 })
+
+@RouteConfig([
+	{
+		path : "/list",
+		name : "List",
+		component : ContactsComponent,
+		useAsDefault: true,
+	},
+	{
+		path : "/detail/:id",
+		name : "Detail",
+		component : ContactDetailComponent,
+	}
+
+])
 
 export class AppComponent{
 	
