94baa41fa417df14cdc379ad1931e7480d21e30e
lakerswgq <2452641634@qq.com>
2016-06-20 19:54:12 +0800

    数据文件联系人数据增加id字段

--- angular2/app/data/contacts.data.ts
+++ angular2/app/data/contacts.data.ts
@@ -2,15 +2,18 @@ import { Contact } from "../class/contact";
 
 export var CONTACTS: Contact[] = [
 
-	{
+	{	
+		id : "1",
 		name: "Julien",
 		phoneNumber: "13964897627"
 	},
 	{
+		id : "2",
 		name: "Jack",
 		phoneNumber: "16955467789",
 	},
 	{
+		id : "3",
 		name: "Billy",
 		phoneNumber: "18922568964"
 	}
