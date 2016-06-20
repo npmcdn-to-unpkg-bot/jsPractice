8505aa33a22580b66cd4e45327aaddd3ee1e5903
lakerswgq <2452641634@qq.com>
2016-06-20 19:55:38 +0800

    联系人列表路由跳转

--- angular2/app/components/contacts.component.ts
+++ angular2/app/components/contacts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from '@angular/router-deprecated';
 
 import { Contact } from "../class/contact";
 import { ContactsService } from "../service/contacts.service";
@@ -6,19 +7,31 @@ import { ContactsService } from "../service/contacts.service";
 @Component({
 	selector : "<contacts></contacts>",
 	templateUrl : "app/templates/contacts.component.html",
+	styleUrls : [
+			"app/style/contacts.component.css",
+		],
 })
 export class ContactsComponent implements OnInit{
 	contacts: Contact[];
 	selectedContact: Contact;
 	constructor (
+			private router: Router,
 			private contactsService: ContactsService
 		){
 
 	}
+	ngOnInit() {
+		this.getContacts();
+	}
+	select (contact){
+		this.selectedContact = contact;
+	}
 	getContacts (){
 		this.contacts = this.contactsService.getContacts();		
 	}
-	ngOnInit (){
-		this.getContacts();
+	gotoDetail (){
+		this.router.navigate(["Detail",{
+			id : this.selectedContact.id
+		}]);
 	}
 }
