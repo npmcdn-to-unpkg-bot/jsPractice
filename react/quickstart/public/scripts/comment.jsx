0c296852a036884324dd0d09e0cb55620bbc773d
lakerswgq <2452641634@qq.com>
2016-07-25 23:54:13 +0800

    comment class

--- react/quickstart/public/scripts/comment.jsx
+++ react/quickstart/public/scripts/comment.jsx
@@ -1,14 +1,54 @@
+
+// comment box
 var CommentBox = React.createClass({
 	render : function (){
 		return (
-			<div className="CommentBox">
-				This is a comment box.
+			<div className="commentBox">
+				<h1>comments</h1>
+				<CommentList></CommentList>
+				<CommentForm></CommentForm>
+			</div>
+		);
+	}
+});
+
+// comment list 
+var CommentList = React.createClass({
+	render : function (){
+		return (
+			<div className="commentList">
+				This is comment list
+			</div>
+		);
+	}
+});
+
+// comment 
+var Comment = React.createClass({
+	render : function (){
+		return (
+			<div className="comment">
+				<h2 className="commentAuthor">
+					{this.props.author}
+				</h2>
+				{this.props.children}
+			</div>
+		);
+	}
+})
+
+// commment form
+var CommentForm = React.createClass({
+	render : function (){
+		return (
+			<div className="commentForm">
+				This is a comment form
 			</div>
 		);
 	}
 });
 
 ReactDOM.render(
-	<CommentBox />,
+	<CommentBox></CommentBox>,
 	document.getElementById("content")
 );
