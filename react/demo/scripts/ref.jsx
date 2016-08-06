17456a0348eb47bbdc3edf8750debb2a27418ea6
lakerswgq <2452641634@qq.com>
2016-08-06 23:59:45 +0800

    jsx script for getting dom element using react refs

--- react/demo/scripts/ref.jsx
+++ react/demo/scripts/ref.jsx
@@ -1,12 +1,17 @@
 var Ref = React.createClass({
-	handleClick : function (){
-		this.refs.hello.innerHTML = "fuck the world";
+	handleClick : function (name){
+		this.refs.hello.innerHTML = name + " said : " + "fuck the world";
 	},
 	render : function (){
 		return (
 			<div>
 				<p ref = "hello">hello,world</p>
-				<button onClick = {this.handleClick}>button</button>
+				{/* es6 */}
+				<button onClick = {() => this.handleClick("julien")}>button</button>
+				{
+					// es5
+					// <button onClick = {this.handleClick.bind(this,"julien")}>button</button>
+				}
 			</div>
 		)
 	}
