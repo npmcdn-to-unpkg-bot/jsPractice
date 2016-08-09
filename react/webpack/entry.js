de0af31dadbbcd0abcdef9dee1d005664adb23a2
lakerswgq <2452641634@qq.com>
2016-08-09 17:14:59 +0800

    modify entry script: import polyfill,jquery package

--- react/webpack/entry.js
+++ react/webpack/entry.js
@@ -1,3 +1,13 @@
 require("./css/style.css");
-var hello = require("./js/hello.jsx");
-console.log(hello);
+
+import "babel-polyfill";
+import cats from "./js/cats.jsx";
+import $ from "jquery";
+
+$("<h1>Cats</h1>").appendTo("body");
+const ul = $("<ul></ul>").appendTo("body");
+for (const cat of cats){
+	$("<li></li>").text(cat).appendTo(ul);
+}
+
+
