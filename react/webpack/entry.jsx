6e7a328e9026c2d5000f2231ae0b155783e5c5f8
lakerswgq <2452641634@qq.com>
2016-08-14 00:02:46 +0800

    split into modules

--- react/webpack/entry.jsx
+++ react/webpack/entry.jsx
@@ -1,53 +1,13 @@
 import React , { Component } from "react";
-import { render as Render, findDOMNode as Find } from "react-dom";
+import { render as Render } from "react-dom";
+import MyComponent from "myComponent";
+import LikeButton from "likeButton";
+import Test from "test";
 
-class LikeButton extends Component {
-	constructor (props){
-		super (props);
-		
-		this.state = {
-			liked : false
-		};
-	}
-	static get defaultProps (){
-		return {
-			title : "video",
-			person : "julien"
-		};
-	}
-	handleClick (event){
-		this.setState({
-			liked : !this.state.liked
-		});
-	}
-
-	render (){
-		let text = this.state.liked ? "like" : "don't like";
-		return (
-			<div>
-				<p> { this.props.person + " " + text } the { this.props.title }</p>
-				<br/>
-				{
-					// React components using ES6 classes no longer autobind this to non React methods.	
-					// use bind to bind contenxt
-				}
-				<button onClick = { this.handleClick.bind(this) }>toggleLike</button>
-			</div>
-		)
-	}
-}
-var Test = React.createClass({
-	render : function (){
-		return (
-			<div>
-				<p>for test</p>
-			</div>
-		);
-	},
-});
 
 Render(
 	<div>
+		<MyComponent></MyComponent>
 		<LikeButton></LikeButton>
 		<Test></Test>
 	</div>,
