78dcaee952435455a31187fe9a93e7e3d211d91c
lakerswgq <2452641634@qq.com>
2016-08-13 23:50:03 +0800

    try export component

---
+++ react/webpack/components/myComponent.jsx
@@ -0,0 +1,17 @@
+import React , { Component } from "react";
+
+class MyComponent extends Component {
+	constructor (props){
+		super(props);
+	}
+
+	render (){
+		return (
+			<div>
+				<p>component</p>
+			</div>
+		)
+	}
+}
+
+export default MyComponent;
