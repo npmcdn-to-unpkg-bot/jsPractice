ae7079ebdcd82d4a809da3443896af707b9485b8
lakerswgq <2452641634@qq.com>
2016-08-14 21:59:52 +0800

    write component using arrow expression

---
+++ react/webpack/components/arrow.jsx
@@ -0,0 +1,12 @@
+// must  import React
+import React from "react";
+
+let Arrow =  (props) => {
+	return (
+		<div>
+			<p>Arrow Component by { props.author }</p>		
+		</div>
+	);
+}
+
+export default Arrow;
