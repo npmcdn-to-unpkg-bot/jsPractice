a540a27942c42b778c097749cf612cb93d57df5f
lakerswgq <2452641634@qq.com>
2016-08-14 22:00:17 +0800

    import new component

--- react/webpack/entry.jsx
+++ react/webpack/entry.jsx
@@ -1,15 +1,17 @@
 import React , { Component } from "react";
 import { render as Render } from "react-dom";
+
 import MyComponent from "myComponent";
 import LikeButton from "likeButton";
 import Test from "test";
-
+import Arrow from "arrow";
 
 Render(
 	<div>
 		<MyComponent></MyComponent>
 		<LikeButton></LikeButton>
 		<Test></Test>
+		<Arrow author = {"julien"}></Arrow>
 	</div>,
 	document.getElementById("content")
 );
