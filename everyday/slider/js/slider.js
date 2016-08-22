73894d1675c3be48929a7495fce160e686c68c98
lakerswgq <2452641634@qq.com>
2016-08-22 15:00:37 +0800

    make it better

--- everyday/slider/js/slider.js
+++ everyday/slider/js/slider.js
@@ -1,26 +1,50 @@
 ;(function(window, document){
 	var sliderImg = document.getElementsByClassName("sliderImg"),
+		sliderLeft = document.getElementById("sliderLeft"),
+		sliderRight = document.getElementById("sliderRight"),
 		sliderIndex = 0,
-		_sliderNumber = 4;
+		_sliderNumber = 4,
+		clearFlag = null;
 
-	var showSlider = function(){
-		if (sliderIndex > _sliderNumber || sliderIndex < 0 ){
+	var showSlider = function(n){
+		var length = sliderImg.length;
+		if (n === 1){
+			clearTimeout(clearFlag);
+			showSlider();
+			return;
+		}
+		else if (n === -1){
+			clearTimeout(clearFlag);
+			sliderIndex = sliderIndex - 2;
+			showSlider();
+			return;
+		}
+
+		if (sliderIndex > _sliderNumber){
 			sliderIndex = 0;
 		}
-
-		if (sliderIndex === 0){
-			sliderImg[_sliderNumber].style.display = "none";
+		else if (sliderIndex < 0){
+			sliderIndex = _sliderNumber;
 		}
-		else {
-			sliderImg[sliderIndex -1].style.display = "none";
+
+		for (var i=0; i<length; i++){
+			sliderImg[i].style.display = "none";
 		}
 
 		sliderImg[sliderIndex].style.display = "block";
 		sliderIndex++
 
-		setTimeout(showSlider, 2000);
+		clearFlag = setTimeout(showSlider, 2000);
 
 	}
 
 	showSlider();
+
+	sliderLeft.addEventListener("click", function (){
+		showSlider(-1);
+	});
+
+	sliderRight.addEventListener("click",function (){
+		showSlider(1);
+	});
 })(window, document);
