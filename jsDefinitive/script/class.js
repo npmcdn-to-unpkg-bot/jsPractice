a39f09d7f98769dd71e1414a7437eb2bb08eab02
lakerswgq <2452641634@qq.com>
2016-08-28 15:31:34 +0800

    try class inheritance in ecmascript5

--- jsDefinitive/script/class.js
+++ jsDefinitive/script/class.js
@@ -10,3 +10,10 @@ Person.prototype.sayHello = function(){
 var julien = new Person("julien");
 julien.sayHello();
 
+var Student = function (name, grade){
+	Person.call(this, name);
+	this.grade = grade;
+}
+Student.prototype = Object.create(Person.prototype);
+var jk= new Student("highSchoolGirl", 10);
+jk.sayHello();
