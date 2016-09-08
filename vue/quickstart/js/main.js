e4d23ff67a5e80a35b2cb56efa3af99b05cf8063
lakerswgq <2452641634@qq.com>
2016-09-08 10:14:34 +0800

    vue todo list

--- vue/quickstart/js/main.js
+++ vue/quickstart/js/main.js
@@ -2,7 +2,24 @@
 	var app = new Vue({
 		el : "#app",
 		data : {
-			message : "hello,world"
+			newTodo : "",
+			todos : [
+				{
+					text  : "test todo text"
+				}
+			],
+		},
+		methods : {
+			addNewTodo : function (){
+				if (this.newTodo){
+					this.todos.push({
+						text : this.newTodo
+					});
+				}
+			},
+			removeTodo : function (index){
+				this.todos.splice(index, 1);
+			}
 		}
 	});
 })(window, document, Vue);
